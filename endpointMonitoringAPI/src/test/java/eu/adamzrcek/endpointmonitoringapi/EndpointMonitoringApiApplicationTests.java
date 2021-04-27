7fa575126a1a8afa02d470ef453d512017d0a8d5
Adam Zrcek <adamzrc@gmail.com>
2021-04-27 09:57:24 +0200

    initial commit

---
+++ endpointMonitoringAPI/src/test/java/eu/adamzrcek/endpointmonitoringapi/EndpointMonitoringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.adamzrcek.endpointmonitoringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndpointMonitoringApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
