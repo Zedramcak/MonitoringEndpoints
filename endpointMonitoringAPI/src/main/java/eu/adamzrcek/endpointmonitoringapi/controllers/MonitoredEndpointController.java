61e34c7e53b9e0228e71b3052bb3bc04b166e7f3
Adam Zrcek <adamzrc@gmail.com>
2021-04-28 20:00:56 +0200

    create controller for MonitoredEndpoint

---
+++ endpointMonitoringAPI/src/main/java/eu/adamzrcek/endpointmonitoringapi/controllers/MonitoredEndpointController.java
@@ -0,0 +1,86 @@
+package eu.adamzrcek.endpointmonitoringapi.controllers;
+
+import eu.adamzrcek.endpointmonitoringapi.models.MonitoredEndpoint;
+import eu.adamzrcek.endpointmonitoringapi.services.MonitoredEndpointService;
+import eu.adamzrcek.endpointmonitoringapi.services.MonitoringResultService;
+import eu.adamzrcek.endpointmonitoringapi.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/monitoredEndpoint")
+public class MonitoredEndpointController {
+
+    private final MonitoredEndpointService monitoredEndpointService;
+    private final MonitoringResultService monitoringResultService;
+    private final UserService userService;
+    private Logger logger = LoggerFactory.getLogger(MonitoredEndpointController.class);
+
+    public MonitoredEndpointController(MonitoredEndpointService monitoredEndpointService, MonitoringResultService monitoringResultService, UserService userService) {
+        this.monitoredEndpointService = monitoredEndpointService;
+        this.monitoringResultService = monitoringResultService;
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    List<MonitoredEndpoint> getAllEndpoints(@RequestHeader(value = "accessToken") String accessToken){
+        return monitoredEndpointService.getMonitoredEndpointsForUser(userService.getUserByToken(accessToken));
+    }
+
+    @PostMapping("/")
+    MonitoredEndpoint addNewEndpoint(@RequestHeader(value = "accessToken") String accessToken,
+                                     @RequestBody MonitoredEndpoint newMonitoredEndpoint){
+        newMonitoredEndpoint.setOwner(userService.getUserByToken(accessToken));
+        return monitoredEndpointService.createNewMonitoredEndpoint(newMonitoredEndpoint);
+    }
+
+    @GetMapping("/{id}")
+    MonitoredEndpoint getMonitoredEndpoint(@PathVariable String id,
+                                           @RequestHeader(value = "accessToken") String accessToken){
+        MonitoredEndpoint getEndpoint = monitoredEndpointService.getMonitoredEndpoint( Integer.parseInt(id));
+
+
+        if (getEndpoint.getOwner().equals(userService.getUserByToken(accessToken))){
+            logger.info("200 " + getEndpoint);
+            return getEndpoint;
+        }
+        else throw new UnauthorizedException("read");
+    }
+
+    @PutMapping("/{id}")
+    MonitoredEndpoint updateMonitoredEndpoint(@RequestHeader(value = "accessToken") String accessToken,
+                                              @PathVariable String id,
+                                              @RequestBody MonitoredEndpoint monitoredEndpointToUpdate){
+
+        MonitoredEndpoint monitoredEndpoint = monitoredEndpointService.getMonitoredEndpoint(Integer.parseInt(id));
+
+        if (monitoredEndpoint.getOwner().equals(userService.getUserByToken(accessToken))) {
+            return monitoredEndpointService.updateMonitoredEndpoint(Integer.parseInt(id), monitoredEndpointToUpdate);
+        }
+        else throw new UnauthorizedException("update");
+    }
+
+    @DeleteMapping("/{id}")
+    void deleteMonitoredEndpoint(@PathVariable String id, @RequestHeader(value = "accessToken") String accessToken){
+        int parsedId = Integer.parseInt(id);
+        MonitoredEndpoint endpointToDelete = monitoredEndpointService.getMonitoredEndpoint(parsedId);
+
+        if (endpointToDelete.getOwner().equals(userService.getUserByToken(accessToken)))
+            monitoredEndpointService
+                    .deleteMonitoredEndpoint(monitoredEndpointService.getMonitoredEndpoint(parsedId));
+        else throw new UnauthorizedException("delete");
+    }
+
+
+    @ResponseStatus(value = HttpStatus.FORBIDDEN)
+    private static class UnauthorizedException extends RuntimeException {
+        public UnauthorizedException(String message){
+            super(String.format("You are not allowed to %S this endpoint", message));
+        }
+
+    }
+}
