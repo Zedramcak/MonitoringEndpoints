639e4cb4b938d497a7331c78635dfe9ffc39e093
Adam Zrcek <adamzrc@gmail.com>
2021-04-30 17:03:31 +0200

    Changed the main folder of git

--- endpointMonitoringAPI/src/main/java/eu/adamzrcek/endpointmonitoringapi/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package eu.adamzrcek.endpointmonitoringapi.repositories;
-
-import eu.adamzrcek.endpointmonitoringapi.models.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-    User getUserByAccessToken(String token);
-}
